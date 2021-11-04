a2ef857966061b858bdac1686e06d559ab746bd9
Luana <luanagama8@gmail.com>
2021-11-03 21:23:52 -0300

    Primeiro commit

---
+++ src/main/java/com/luana/springexemplo/SpringExemploApplication.java
@@ -0,0 +1,30 @@
+package com.luana.springexemplo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SpringExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExemploApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args -> {
+			System.out.println("testando o Bean do spring");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for(String beanName: beanNames){
+				System.out.println(beanName);
+			}
+		};
+	}
+}
---
+++ src/test/java/com/luana/springexemplo/SpringExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luana.springexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
